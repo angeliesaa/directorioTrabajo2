8c2834f04525b5d3c2666ae80c5d761dc04dccd3
ancresgon <61710668+ancresgon@users.noreply.github.com>
2024-04-03 09:52:36 +0200

    Segunda fase proyecto (Clase1 creada y Pricipal creada y comentada)

--- src/main/java/directorioTrabajo2/Principal.java
+++ src/main/java/directorioTrabajo2/Principal.java
@@ -1,15 +1,15 @@
 /**
- * 
+ * contiene todas las clases de la aplicacion
  */
 package directorioTrabajo2;
 
 /**
- * 
+ * articula la aplicacion
  */
 public class Principal {
 
 	/**
-	 * @param args
+	 * @param args no recibe argumentos desde linea de comandos
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
